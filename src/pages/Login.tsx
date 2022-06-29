64b32cc67b2530f7bbb023f63417966ac83346a9
Omar Ashraf <33941687+omarattia3143@users.noreply.github.com>
2022-06-29 14:14:49 +0200

    add login and register functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
 import "../styles/Login.css";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [navigate, setNavigate] = useState(false);
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    await axios.post(
+      "login",
+      {
+        email,
+        password,
+      },
+      { withCredentials: true }
+    );
+    setNavigate(true);
+  };
+
+  if (navigate) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <main className="form-signin w-100 m-auto">
-      <form>
+      <form onSubmit={submit}>
         <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
         <div className="form-floating">
@@ -13,6 +36,7 @@ const Login = () => {
             className="form-control"
             id="floatingInput"
             placeholder="name@example.com"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label htmlFor="floatingInput">Email address</label>
         </div>
@@ -22,16 +46,12 @@ const Login = () => {
             className="form-control"
             id="floatingPassword"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
           <label htmlFor="floatingPassword">Password</label>
         </div>
 
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me" /> Remember me
-          </label>
-        </div>
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
+        <button className="w-100 btn btn-lg btn-primary mt-3" type="submit">
           Sign in
         </button>
         <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
